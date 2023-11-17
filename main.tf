e15a12b060528c3740a8e040854f0ddfc0a67d94
root <root@icl00ud-desktop>
2023-11-16 23:40:42 -0300

    Added ansible

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0dbc3d7bc646e8516"
+  ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
   key_name = "iac-alura"
   tags = {
-    Name = "My first instance"
+    Name = "Teste AWS"
   }
 }
